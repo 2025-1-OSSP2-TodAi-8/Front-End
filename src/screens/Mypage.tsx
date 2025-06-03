cafb24fb689aa413a0a43d721af7071da93d5036
EH-OI <kimtae21004@naver.com>
2025-06-03 14:11:08 +0900

    마이페이지_수정

--- src/screens/Mypage.tsx
+++ src/screens/Mypage.tsx
@@ -1,127 +1,94 @@
-// import React, {useState, useEffect} from 'react';
-// import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, View, Alert, ActivityIndicator } from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useState, useEffect} from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, View, Alert, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import API from '../api/api';
+import { FlatList } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-// interface User{
-//   user_id: number;
-//   name: string;
-//   sharing: boolean|null;
-// }
+type RootStackParamList={
+  Alert: undefined;
+};
 
-// function Mypage({navigation}: {navigation: any}) {
-//   const [userInfo, setuserInfo]=useState<User|null>(null);
-//   const [loading, setLoading]=useState(true);
+const Mypage=({navigation}:{navigation: any})=>{
+  const [userInfo, setUserInfo]=useState<null|{
+    user_id: number;
+    name: string;
+    sharing: any[]|null;
+  }>(null);
+  const [loading, setLoading]=useState(true);
 
-//     useEffect(()=>{
-//     fetchUser();
-//     }, []);
+const fetchUser=async()=>{
+  try{
+    const token=await AsyncStorage.getItem('acess');
+    if(!token){
+      Alert.alert('token error', 'need login');
+      return;
+    }
 
-//   const fetchUser=async()=>{
-//     try{
-//       const token=await AsyncStorage.getItem('accessToken');
-//       if(!token) {
-//         Alert.alert('로그인', '접근 권한 없음');
-//         return;
-//       }
-//       const response=await fetch('/api/people', {
-//         method: 'GET', 
-//         headers: {
-//           Authorization: `Bearer ${token}`, 
-//           'Content-Type': 'application/json', 
-//         }, 
-//       }); 
-//       if(!response.ok) throw new Error('error');
-
-//       const data: User =await response.json();
-//       setuserInfo(data);
-//     }
-//     catch(error){
-//       console.error('userinfo error ', error);
-//     }
-//     finally {setLoading(false);}
-//   };
-
-//   if(loading){
-//      return (<View style={styles.center}>
-//       <ActivityIndicator size="large" color='#fff'/>
-//      </View>);
-//   }
-
-//   if(!userInfo){
-//     return (<View style={styles.center}>
-//       <Text>사용자 정보 없음</Text>
-//     </View>);
-//   }
-
-//   const sharingText=userInfo?.sharing===null?'설정되지 않음':userInfo?.sharing?'공유중':'공유하지 않음';
-
-//   return(
-//     <SafeAreaView style={styles.container}>
-//       <View style={styles.header}>
-//             <Text style={styles.title}>내 정보</Text>
-//             <Text style={styles.label}>유저ID<Text style={styles.value}>{userInfo.user_id}</Text></Text>
-//                 <TouchableOpacity style={styles. menu} onPress={()=>navigation.openDrawer()}>
-//                       <Image source={require('./assets/menu.png')} style={styles.menu} /> 
-//                 </TouchableOpacity>
-//       </View>
-//     </SafeAreaView>
-//   );
-// }
-// const styles=StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#f5e0ff',
-//     alignItems: 'center',
-//   },
-//   header: {
-//     flexDirection: 'row', 
-//     alignItems: 'center', 
-//     marginTop: 50, 
-//   },
-//   title: {
-//     fontSize: 25,
-//     fontWeight: '500',
-//     color: '#531ea3',
-//   },
-//   menu:{
-//     position: 'absolute',
-//     width: 30, 
-//     height: 45,
-//     right: 95,
-//     resizeMode: 'contain',
-//   },
-//   label: {
-//     fontSize: 25,
-//     fontWeight: '500',
-//     color: '#531ea3',
-//   }, 
-//   value:{
-//     fontSize: 25,
-//     fontWeight: '500',
-//     color: '#531ea3',
-//   }, 
-//   center:{
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-// export default Mypage;
-
-import React from 'react';
-import { View, TouchableOpacity, SafeAreaView, Text, StyleSheet, Image } from 'react-native';
-import { useNavigation, DrawerActions } from '@react-navigation/native';
-
-function Mypage({navigation}: {navigation: any}) {
+    const response=await API.get('/api/people', {
+      headers:{
+        Authorization: `Bearer ${token}`, 
+      }, 
+    });
+    setUserInfo(response.data);
+  }
+  catch(error){
+    console.error('user info error', error);
+    Alert.alert('error', 'mypage error');
+  }
+  finally{setLoading(false);}
+};
+useEffect(()=>{
+  fetchUser();
+}, []);
+if(loading){
   return(
-    <SafeAreaView style={styles.container}>
-          <Text style={styles.title}>내 정보</Text>
-          <TouchableOpacity style={styles. menu} onPress={()=>navigation.openDrawer()}>
-              <Image source={require('./assets/menu.png')} style={styles.menu} /> 
-          </TouchableOpacity>
-    </SafeAreaView>
+    <View style={styles.center}>
+      <ActivityIndicator size="large" color="#888"/>
+    </View>  
   );
 }
+if(!userInfo){
+  return(
+    <View style={styles.center}>
+      <Text>유저 정보를 불러올 수 없습니다. </Text>
+    </View>
+  );
+}
+return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles. menu} onPress={()=>navigation.openDrawer()}>
+        <Image source={require('./assets/menu.png')} style={styles.menu} /> 
+      </TouchableOpacity>
+      <TouchableOpacity style={styles. alert} onPress={()=>navigation.navigate('Alert')}>
+        <Image source={require('./assets/alert.png')} style={styles.menu} /> 
+      </TouchableOpacity>
+      <Text style={styles.title}>마이페이지</Text>
+      <Text style={styles.name}>이름: {userInfo.name}</Text>
+      <Text style={styles.name}>유저 ID: {userInfo.user_id}</Text>
+
+      <Text style={styles.sectionTitle}>현재 연동된 보호자</Text>
+      {userInfo.sharing && userInfo.sharing.length > 0 ? (
+        <FlatList
+          data={userInfo.sharing}
+          keyExtractor={(item, index) => `${item.protector_id}-${index}`}
+          renderItem={({ item }) => (
+            <View style={styles.itemBox}>
+              <Text>이름: {item.protector_name}</Text>
+              <Text>공개범위: {item.공개범위}</Text>
+            </View>
+          )}
+        />
+      ) : (
+        <Text style={styles.subText}>연동된 보호자가 없습니다.</Text>
+      )}
+
+    </View>
+  );
+}
+export default Mypage;
+
 const styles=StyleSheet.create({
   container: {
     flex: 1,
@@ -135,12 +102,86 @@ const styles=StyleSheet.create({
     color: '#531ea3',
     top: 60,
   },
-  menu: {
+  name: {
     position: 'absolute',
     width: 30, 
     height: 140,
     right: 180,
     resizeMode: 'contain',
-  }
+  }, 
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 20,
+    marginBottom: 8,
+
+  }, 
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }, 
+  itemBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }, 
+  subText: {
+    fontSize: 14,
+    color: '#999',
+    marginBottom: 10,
+  }, 
+  menu: {
+    position: 'absolute',
+    width: 30, 
+    height: 45,
+    right: 100,
+    resizeMode: 'contain',
+  }, 
+  alert: {
+    position: 'absolute',
+    width: 30, 
+    height: 45,
+    right: 100,
+    resizeMode: 'contain',
+  }, 
 });
-export default Mypage;
+
+// import React from 'react';
+// import { View, TouchableOpacity, SafeAreaView, Text, StyleSheet, Image } from 'react-native';
+// import { useNavigation, DrawerActions } from '@react-navigation/native';
+
+// function Mypage({navigation}: {navigation: any}) {
+//   return(
+//     <SafeAreaView style={styles.container}>
+//           <Text style={styles.title}>내 정보</Text>
+//           <TouchableOpacity style={styles. menu} onPress={()=>navigation.openDrawer()}>
+//               <Image source={require('./assets/menu.png')} style={styles.menu} /> 
+//           </TouchableOpacity>
+//     </SafeAreaView>
+//   );
+// }
+// const styles=StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f5e0ff',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: '500',
+//     color: '#531ea3',
+//     top: 60,
+//   },
+//   menu: {
+//     position: 'absolute',
+//     width: 30, 
+//     height: 140,
+//     right: 180,
+//     resizeMode: 'contain',
+//   }
+// });
+// export default Mypage;
